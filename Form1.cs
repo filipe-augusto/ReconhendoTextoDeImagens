4a87fd4db1e7c858d1a44fd79ff86bd097cdf95a
filipe augusto <67299107+filipe-augusto@users.noreply.github.com>
2022-11-08 17:22:06 -0300

    ASPOSE

--- Form1.cs
+++ Form1.cs
@@ -43,6 +43,22 @@ namespace ReconhendoTextoDeImagens
             }
         }
 
+        private void button2_Click(object sender, EventArgs e)  
+        {
+            Aspose.OMR.Api.OmrEngine omrEngine = new Aspose.OMR.Api.OmrEngine();
+            Aspose.OMR.Generation.GenerationResult generationResult = omrEngine.GenerateTemplate("template3.txt");
+            generationResult.Save("", "PROVA.OMR");
+        }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Aspose.OMR.Api.OmrEngine omrEngine = new Aspose.OMR.Api.OmrEngine();
+            Aspose.OMR.Api.TemplateProcessor templateProcessor = omrEngine.GetTemplateProcessor("PROVA.OMR.omr");
+            Aspose.OMR.Model.RecognitionResult recognitionResult = templateProcessor.RecognizeImage("asd.png");
+            string result = recognitionResult.GetCsv();
+            txtLido.Text += "\n" + result;
+            Console.WriteLine(result);
+
+        }
     }
 }
