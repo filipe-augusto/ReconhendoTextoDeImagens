3931a476817e6d49df94d594d8d64de30682146e
filipe augusto <67299107+filipe-augusto@users.noreply.github.com>
2022-11-08 18:11:58 -0300

    add files

--- Form1.cs
+++ Form1.cs
@@ -46,15 +46,15 @@ namespace ReconhendoTextoDeImagens
         private void button2_Click(object sender, EventArgs e)  
         {
             Aspose.OMR.Api.OmrEngine omrEngine = new Aspose.OMR.Api.OmrEngine();
-            Aspose.OMR.Generation.GenerationResult generationResult = omrEngine.GenerateTemplate("template3.txt");
-            generationResult.Save("", "PROVA.OMR");
+            Aspose.OMR.Generation.GenerationResult generationResult = omrEngine.GenerateTemplate("tearline.txt");
+            generationResult.Save("", "t1.OMR");
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             Aspose.OMR.Api.OmrEngine omrEngine = new Aspose.OMR.Api.OmrEngine();
-            Aspose.OMR.Api.TemplateProcessor templateProcessor = omrEngine.GetTemplateProcessor("PROVA.OMR.omr");
-            Aspose.OMR.Model.RecognitionResult recognitionResult = templateProcessor.RecognizeImage("asd.png");
+            Aspose.OMR.Api.TemplateProcessor templateProcessor = omrEngine.GetTemplateProcessor("tearline.omr");
+            Aspose.OMR.Model.RecognitionResult recognitionResult = templateProcessor.RecognizeImage("prova1.png");
             string result = recognitionResult.GetCsv();
             txtLido.Text += "\n" + result;
             Console.WriteLine(result);
